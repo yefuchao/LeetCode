ca0c5de3dcbb92b86dbcd41d3964bf43e8c6f99d
alex@coding <ye0531@live.com>
2018-02-01 22:58:57 +0800

    2018.2.1

---
+++ 3Sum Closest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Sum_Closest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 1, 1, 0 };
+
+            Console.WriteLine(ThreeSumCloest(nums, 100));
+            Console.ReadLine();
+        }
+
+        public static int ThreeSumCloest(int[] nums, int target)
+        {
+            //todo
+            Array.Sort(nums);
+            var cloest = Math.Abs(nums[nums.Length - 1] + nums[nums.Length - 2] + nums[nums.Length - 3] - target);
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                var j = 1;
+                while (i + j + 1 < nums.Length)
+                {
+                    cloest = Math.Min(Math.Abs(nums[i] + nums[i + j] + nums[i + j + 1] - target), cloest);
+                    j++;
+                }
+            }
+            return cloest + target;
+        }
+    }
+}
--- 3Sum/Program.cs
+++ 3Sum/Program.cs
@@ -10,14 +10,65 @@ namespace _3Sum
     {
         static void Main(string[] args)
         {
+            int[] nums = { -4, -1, -1, 0, 1, 2 };
+
+            var t = ThreeSum(nums);
+
+            Console.ReadLine();
         }
 
-        //public static IList<IList<int>> ThreeSum(int[] nums)
-        //{
-            //IList<IList<int>> result ;
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
 
+            Array.Sort(nums);
 
-            //return result;
-        //}
+            var len = nums.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                var font = i + 1;
+
+                var back = len - 1;
+
+                var tar = -nums[i];
+
+                while (back > font)
+                {
+                    var sum = nums[font] + nums[back];
+
+                    if (sum > tar)
+                    {
+                        back--;
+                    }
+                    else if (sum < tar)
+                    {
+                        font++;
+                    }
+                    else
+                    {
+                        List<int> list = new List<int> { nums[i], nums[font], nums[back] };
+                        result.Add(list);
+
+                        while (font < back && nums[font] == list[1])
+                        {
+                            font++;
+                        }
+
+                        while (font < back && nums[back] == list[2])
+                        {
+                            back--;
+                        }
+                    }
+                }
+
+                while (i + 1 < len && nums[i + 1] == nums[i])
+                {
+                    i++;
+                }
+            }
+
+            return result;
+        }
     }
 }
