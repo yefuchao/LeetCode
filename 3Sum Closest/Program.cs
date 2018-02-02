6c2cd38a0db9b4b6e5685e3e58c68a913834c9ec
yefuchao <ye0531@live.com>
2018-02-02 19:09:41 +0800

    2018.2.2 todo

--- 3Sum Closest/Program.cs
+++ 3Sum Closest/Program.cs
@@ -18,19 +18,101 @@ namespace _3Sum_Closest
 
         public static int ThreeSumCloest(int[] nums, int target)
         {
-            //todo
             Array.Sort(nums);
+
             var cloest = Math.Abs(nums[nums.Length - 1] + nums[nums.Length - 2] + nums[nums.Length - 3] - target);
-            for (int i = 0; i < nums.Length - 2; i++)
+
+            var a = 0;
+            var b = 0;
+            var c = 0;
+
+            var len = nums.Length;
+
+            for (int i = 0; i < nums.Length; i++)
             {
-                var j = 1;
-                while (i + j + 1 < nums.Length)
+                var font = i + 1;
+                var back = len - 1;
+
+                while (back > font)
                 {
-                    cloest = Math.Min(Math.Abs(nums[i] + nums[i + j] + nums[i + j + 1] - target), cloest);
-                    j++;
+                    var sum = nums[i] + nums[font] + nums[back];
+
+                    var distance = Math.Abs(sum - target);
+
+                    if (distance <= cloest)
+                    {
+                        a = nums[i];
+                        b = nums[font];
+                        c = nums[back];
+                        cloest = distance;
+                    }
+
+                    if (sum < target)
+                    {
+                        font++;
+                    }
+                    else if (sum > target)
+                    {
+                        back--;
+                    }
+                    else
+                    {
+                        return target;
+                    }
                 }
             }
-            return cloest + target;
+
+            return a + b + c;
+        }
+
+        public static int ThreeSumCloese(int[] nums,int target)
+        {
+
+            Array.Sort(nums);
+            long result = Int32.MaxValue;
+
+            for (int i = 0; i + 2 < nums.Length; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+
+                int j = i + 1;
+                int k = nums.Length - 1;
+
+                while (j < k)
+                {
+                    result = Math.Abs(result - target) > Math.Abs(nums[i] + nums[j] + nums[k] - target) ? nums[i] + nums[j] + nums[k] : result;
+
+                    if (nums[i] + nums[j] + nums[k] > target)
+                    {
+                        k--;
+
+                        if (j < k && nums[k] == nums[k + 1])
+                        {
+                            k--;
+                        }
+                    }
+                    else if (nums[i] + nums[j] + nums[k] < target)
+                    {
+                        j++;
+
+                        if (j < k && nums[j] == nums[j - 1])
+                        {
+                            j++;
+                        }
+                    }
+                    else
+                    {
+                        return (int)result;
+                    }
+                }
+            }
+
+            return (int)result;
+
+
         }
     }
 }
---
+++ 4Sum/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -5, 5, 4, -3, 0, 0, 4, -2 }; // 0
+
+            // nums = new int[] { -2, -1, 0, 0, 1, 2 }; //
+
+            //nums = new int[] { 5, 5, 3, 5, 1, -5, 1, -2 }; //4
+
+            nums = new int[] { 0, 0, 0, 0 };
+
+            var t = FourSum(nums, 0);
+
+            Console.ReadLine();
+        }
+
+        public static IList<IList<int>> FourSum(int[] nums, int target)
+        {
+
+            //todo
+
+            IList<IList<int>> result = new List<IList<int>>();
+
+            Array.Sort(nums);
+
+            var len = nums.Length;
+
+            for (int i = 0; i < len - 3; i++)
+            {
+                for (int j = len - 1; j >= 0; j--)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+
+                    var font = i + 1;
+
+                    var back = j - 1;
+
+                    while (back > font)
+                    {
+                        var sum = nums[i] + nums[j] + nums[font] + nums[back];
+
+                        if (sum < target)
+                        {
+                            font++;
+                        }
+                        else if (sum > target)
+                        {
+                            back--;
+                        }
+                        else
+                        {
+                            List<int> list = new List<int> { nums[i], nums[j], nums[font], nums[back] };
+
+                            result.Add(list);
+
+                            font++;
+                            back--;
+
+                            while (nums[font] == list[2] && font < back)
+                            {
+                                font++;
+                            }
+
+                            while (nums[back] == list[3] && back > font)
+                            {
+                                back--;
+                            }
+                        }
+
+                    }
+
+                    while (j > 2 && nums[j] == nums[j - 1] && j > i)
+                    {
+                        j--;
+                    }
+                }
+
+                while (nums[i] == nums[i++] && i < len - 3)
+                {
+                    i++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
