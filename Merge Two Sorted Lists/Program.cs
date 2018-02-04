08c697fc2152b7b652591013d0c1c565f7082282
alex@coding <ye0531@live.com>
2018-02-04 22:45:18 +0800

    Merge_Two_Sorted_Lists

---
+++ Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Two_Sorted_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l2 == null)
+            {
+                return l1;
+            }
+
+            if (l1 == null)
+            {
+                return l2;
+            }
+
+            if (l1.val < l2.val)
+            {
+                l1.next = MergeTwoLists(l1.next, l2);
+                return l1;
+            }
+            else
+            {
+                l2.next = MergeTwoLists(l2.next, l1);
+                return l2;
+            }
+        }
+
+
+
+        public class ListNode
+        {
+            public int val { get; set; }
+
+            public ListNode next { get; set; }
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
---
+++ Remove Nth Node From End of List/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Nth_Node_From_End_of_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            if (head.next == null && n == 0)
+            {
+                return null;
+            }
+
+            ListNode list = null;
+
+            var count = 0;
+
+            list = new ListNode(head.val);
+
+            while (head.next != null)
+            {
+
+                if (count == n)
+                {
+
+                }
+
+                count++;
+            }
+
+            return list;
+        }
+
+        public ListNode GenerateListNode(ListNode head, int next)
+        {
+            head.next = new ListNode(next);
+
+            return head;
+        }
+
+        public class ListNode
+        {
+            public int val { get; set; }
+
+            public ListNode next { get; set; }
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
