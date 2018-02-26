1fdfc6f23175528e43ca0f01ea9d57c1ceb45e8f
alex@coding <ye0531@live.com>
2018-02-26 22:09:17 +0800

    Add MyQueue and UnitTest

---
+++ LeetCodeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeetCodeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCodeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd1b49de-e132-4376-ab67-00e2bef756b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LeetCodeTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_Queue;
+
+namespace LeetCodeTest
+{
+    [TestClass]
+    public class MyQueueTest
+    {
+        [TestMethod]
+        public void TestMyQueue()
+        {
+            MyQueue queue = new MyQueue();
+
+            queue.Push(1);
+            queue.Push(2);
+            queue.Push(3);
+
+            Assert.AreEqual(queue.Pop(), 1);
+            Assert.AreEqual(queue.Peek(), 2);
+            Assert.AreEqual(queue.Empty(), false);
+        }
+    }
+}
