f07b83b8e0c15b5e4befe0246e97f1ced34fcc3c
alex@coding <ye0531@live.com>
2018-02-26 22:33:51 +0800

    Question MyStack

---
+++ LeetCodeTest/MyStacktest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using My_Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTest
+{
+    [TestClass]
+    public class MyStackTest
+    {
+        [TestMethod]
+        public void TestMyStack()
+        {
+            MyStack stack = new MyStack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Assert.AreEqual(stack.Pop(), 3);
+            Assert.AreEqual(stack.Top(), 2);
+            Assert.AreEqual(stack.Empty(), false);
+
+        }
+    }
+}
---
+++ MyQueue/MyStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Queue
+{
+    public class MyStack
+    {
+        Queue<int> q1;
+        Queue<int> q2;
+
+        public MyStack()
+        {
+            q1 = new Queue<int>();
+            q2 = new Queue<int>();
+        }
+
+        public void Push(int x)
+        {
+            while (q1.Count > 0)
+            {
+                q2.Enqueue(q1.Dequeue());
+            }
+
+            q1.Enqueue(x);
+
+
+            while (q2.Count > 0)
+            {
+                q1.Enqueue(q2.Dequeue());
+            }
+        }
+
+        public int Pop()
+        {
+            return q1.Dequeue();
+        }
+
+        public int Top()
+        {
+            return q1.Peek();
+        }
+
+        public bool Empty()
+        {
+            return q1.Count == 0;
+        }
+    }
+}
