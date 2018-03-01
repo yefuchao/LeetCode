87dad250450d769bdf4d4bcd7f7e9fa71ec9c376
alex@coding <ye0531@live.com>
2018-03-01 23:09:54 +0800

    Sqrt(x)

---
+++ Sqrt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sqrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MySqrt(10));
+
+            Console.ReadLine();
+        }
+
+        public static int MySqrt(int x)
+        {
+            int left = 1, right = x;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (mid == x / mid)
+                {
+                    return mid;
+                }
+                else if (mid < x / mid)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return right;
+        }
+        
+    }
+}
