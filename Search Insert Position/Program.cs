6c64e883dacba909ac0e11d74076b99193846683
alex@coding <ye0531@live.com>
2018-03-04 00:13:52 +0800

    Search Insert Postion

--- Search Insert Position/Program.cs
+++ Search Insert Position/Program.cs
@@ -10,62 +10,24 @@ namespace Search_Insert_Position
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[] { 1, 2, 3, 4, 5, 7 };
-            Console.WriteLine(SerachInsert(nums, 8));
+            int[] nums = new int[] { 1, 3, 5, 6 };
+            Console.WriteLine(SerachInsert(nums, 2));
             Console.ReadLine();
         }
 
         public static int SerachInsert(int[] nums, int target)
         {
-            if (nums.Length == 0)
-            {
-                return 0;
-            }
+            var pos = nums.Length;
 
-            if (nums.Length == 1)
+            for (int i = 0; i < nums.Length; i++)
             {
-                if (nums[0] >= target)
+                if (target <= nums[i])
                 {
-                    return 0;
-                }
-                else
-                {
-                    return 1;
+                    return target = i;
                 }
             }
 
-            int left = 0;
-            int right = nums.Length - 1;
-
-            while (left < right)
-            {
-                if (target > nums[right])
-                {
-                    return right + 1;
-                }
-
-                if (target < nums[left])
-                {
-                    return left;
-                }
-
-                var mid = left + (right - left) / 2;
-
-                if (target == nums[mid])
-                {
-                    return mid;
-                }
-                else if (target > nums[mid])
-                {
-                    left = mid + 1;
-                }
-                else
-                {
-                    right = mid + 1;
-                }
-            }
-
-            return right;
+            return pos;
 
         }
 
