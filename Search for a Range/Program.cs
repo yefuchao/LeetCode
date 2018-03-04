0c42235ea2ffda527841ad3d9283395886adb53a
alex@coding <ye0531@live.com>
2018-03-04 22:20:15 +0800

    Search for a range

---
+++ Search for a Range/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_for_a_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 5, 7, 7, 8, 8, 10 };
+            var t = SearchRange(nums, 9);
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+
+        public static int[] SearchRange(int[] nums, int target)
+        {
+            int[] range = new int[] { -1, -1 };
+
+            int leftIndex = extremeInsertionIndex(nums, target, true);
+
+            if (leftIndex == nums.Length || nums[leftIndex] != target)
+            {
+                return range;
+            }
+
+            int rightIndex = extremeInsertionIndex(nums, target, false);
+
+            range[0] = leftIndex;
+            range[1] = rightIndex - 1;
+
+            return range;
+        }
+
+        public static int extremeInsertionIndex(int[] nums, int target, bool left)
+        {
+            int low = 0;
+            int high = nums.Length;
+
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+
+                if (nums[mid] > target || (left && nums[mid] == target))
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return low;
+        }
+
+    }
+}
---
+++ Search in Rotated Sorted Array/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_in_Rotated_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+
+        }
+
+        public int SearchQucik(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0)
+                return -1;
+
+            return BinarySearch(nums, target, 0, nums.Length - 1);
+        }
+
+        private int BinarySearch(int[] nums, int target, int low, int high)
+        {
+            if (low > high)
+                return -1;
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid])
+            {
+                if (target < nums[mid] && target >= nums[low])
+                    return BinarySearch(nums, target, low, mid - 1);
+
+                return BinarySearch(nums, target, mid + 1, high);
+            }
+
+            if (target <= nums[high] && target > nums[mid])
+            {
+                return BinarySearch(nums, target, mid + 1, high);
+            }
+
+            return BinarySearch(nums, target, low, mid - 1);
+        }
+    }
+}
