4329982a8e02ad8cdd9d4c5d404db2e1bc7b38f3
yefuchao <ye0531@live.com>
2018-03-07 18:28:19 +0800

    Best Time To Buy And Sell Stock II

---
+++ Best Time to Buy and Sell Stock II/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Best_Time_to_Buy_and_Sell_Stock_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] prices = { 1, 2 };
+
+            Console.WriteLine(MaxProfit(prices));
+
+            Console.ReadLine();
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+
+            if (prices.Length < 2)
+            {
+                return 0;
+            }
+
+            int max = 0;
+
+            bool hasStock = false;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (hasStock)
+                {
+                    if (prices[i] < prices[i - 1])
+                    {
+                        max += prices[i - 1];
+                        hasStock = false;
+                    }
+                }
+                else
+                {
+                    if (prices[i] > prices[i - 1])
+                    {
+                        max -= prices[i - 1];
+                        hasStock = true;
+                    }
+                }
+            }
+
+
+            if (hasStock)
+            {
+                max += prices[prices.Length - 1];
+            }
+
+            return max;
+        }
+    }
+}
