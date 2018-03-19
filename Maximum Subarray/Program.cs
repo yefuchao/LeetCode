6e1fb8a7a805910656e6fa0359fb8f4a1855d54e
alex@coding <ye0531@live.com>
2018-03-19 23:32:17 +0800

    Maximum_Subarray

---
+++ Maximum Subarray/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Subarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            Console.WriteLine(MaxSubArray(nums));
+
+            Console.ReadLine();
+        }
+
+        public static int MaxSubArray(int[] nums)
+        {
+            if (nums.Length < 0)
+            {
+                return 0;
+            }
+            var max = nums[0];
+            var preMax = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                preMax = Math.Max(nums[i], preMax + nums[i]);
+                max = Math.Max(preMax, max);
+            }
+            return max;
+        }
+    }
+}
