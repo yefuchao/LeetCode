c20bddc2b6e51bb458111c9be3ce4f403c8d5efa
yefuchao <ye0531@live.com>
2019-01-23 17:48:29 +0800

    19.1.23

---
+++ Largest Number/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Largest_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            int[] nums = new int[] { 3, 30, 34, 5, 9 };
+
+            LargestNumber(nums);
+
+            Console.ReadLine();
+        }
+
+
+        public static string LargestNumber(int[] nums)
+        {
+            var result = "";
+            string[] str_nums = new string[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                str_nums[i] = nums[i].ToString();
+            }
+
+            Array.Sort(str_nums, CompareTwoString);
+
+            if (str_nums[0].StartsWith('0'))
+            {
+                return "0";
+            }
+
+            foreach (var item in str_nums)
+            {
+                //Console.WriteLine(item);
+
+                result += item;
+            }
+
+            return result;
+        }
+
+        public static int CompareTwoString(string x, string y)
+        {
+            var str1 = x + y;
+            var str2 = y + x;
+            return str2.CompareTo(str1);
+            //return y.CompareTo(x);
+        }
+
+
+    }
+}
---
+++ Sort List/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Sort_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+
+
+        public class ListNode
+        {
+            public int val;
+
+            public ListNode next;
+
+            public ListNode(int x) { val = x; }
+        }
+
+        public static ListNode SortList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode prev = null, slow = head, fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                prev = slow;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            prev.next = null;
+
+            ListNode l1 = SortList(head);
+            ListNode l2 = SortList(slow);
+
+            return Merge(l1, l2);
+
+        }
+
+        static ListNode Merge(ListNode l1, ListNode l2)
+        {
+            ListNode l = new ListNode(0), p = l;
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    p.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    p.next = l2;
+                    l2 = l2.next;
+                }
+            }
+
+            if (l1 != null)
+            {
+                p.next = l1;
+            }
+            if (l2 != null)
+            {
+                p.next = l2;
+            }
+
+            return l.next;
+        }
+    }
+}
