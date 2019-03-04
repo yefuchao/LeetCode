e5169e5555120382c5efe9505f3a491932dba000
alex <ye0531@live.com>
2019-03-05 00:01:44 +0800

    plus one

---
+++ Plus One/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Plus_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] PlusOne(int[] digits)
+        {
+            var len = digits.Length;
+
+            for (int i = len - 1; i >= 0; i--)
+            {
+                if (digits[i] < 9)
+                {
+                    digits[i]++;
+                    return digits;
+                }
+
+                digits[i] = 0;
+            }
+
+            int[] newNumber = new int[len + 1];
+
+            newNumber[0] = 1;
+
+            return newNumber;
+        }
+    }
+}
