fee546ef458471d42c8d07116e641088ae8d59cf
alex <ye0531@live.com>
2019-04-03 00:44:29 +0800

    977  Squares of a Sorted Array

---
+++ 977 Squares of a Sorted Array/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _977_Squares_of_a_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] SortedSquares(int[] A)
+        {
+            if (A.Length == 0)
+            {
+                return A;
+            }
+
+            int first = 0;
+            int last = A.Length - 1;
+            int[] B = new int[A.Length];
+            var current = last;
+            while (last >= first)
+            {
+                if (A[first] * A[first] > A[last] * A[last])
+                {
+                    B[current--] = A[first] * A[first];
+                    first++;
+                }
+                else
+                {
+                    B[current--] = A[last] * A[last];
+                    last--;
+                }
+            }
+
+            return B;
+        }
+    }
+}
---
+++ Decode Ways/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Decode_Ways
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int NumDecodings(string s)
+        {
+            var ways = 0;
+
+            var nums = s.ToCharArray();
+
+            var preChar = '0';
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+            }
+
+            return ways;
+        }
+
+        public static int[] SortedSquares(int[] A)
+        {
+            if (A.Length == 0)
+            {
+                return A;
+            }
+
+            int first = 0;
+            int last = A.Length - 1;
+            int[] B = new int[A.Length];
+            var current = last;
+            while (last >= first)
+            {
+                if (A[first] * A[first] > A[last] * A[last])
+                {
+                    B[current--] = A[first] * A[first];
+                    first++;
+                }
+                else
+                {
+                    B[current--] = A[last] * A[last];
+                    last--;
+                }
+            }
+
+            return B;
+        }
+    }
+}
