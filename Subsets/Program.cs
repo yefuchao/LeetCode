9608e78852118e647c64d55b3512a81cdd2f7675
yefuchao <ye0531@live.com>
2019-06-10 10:50:43 +0800

    [78] Subsets

---
+++ Subsets/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace Subsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var t = Subsets(new int[] {1,2,3});
+
+            Console.WriteLine(t);
+
+            Console.ReadLine();
+        }
+
+        public static IList<IList<int>> Subsets(int[] nums)
+        {
+            var result = new List<IList<int>>();
+
+            
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var ret = new List<IList<int>>();
+
+                foreach (var item in result)
+                {
+                    var temp = new List<int>();
+                    temp.AddRange(item);
+                    temp.Add(nums[i]);
+
+                    ret.Add(temp);
+                }
+
+                result.AddRange(ret);
+
+                result.Add(new List<int> { nums[i] });
+
+            }
+
+            result.Add(new List<int>());
+
+            return result;
+        }
+
+    }
+}
