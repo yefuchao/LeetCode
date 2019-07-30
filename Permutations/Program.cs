0396c2b3456669a845f009ef0f46e4aa470d5b82
yefuchao <ye0531@live.com>
2019-07-30 10:24:51 +0800

    46. Permutations

--- Permutations/Program.cs
+++ Permutations/Program.cs
@@ -10,18 +10,43 @@ namespace Permutations
     {
         static void Main(string[] args)
         {
+            var list =   Permute(new int[] { 1, 2, 3 });
+
+            Console.WriteLine(list.Count());
+            Console.ReadLine();
         }
 
         public static IList<IList<int>> Permute(int[] nums)
         {
             List<IList<int>> list = new List<IList<int>>();
 
-            for (int i = 0; i < nums.Length; i++)
+            if (nums.Length == 0)
             {
-                //todo
+                return list;
+            }
+
+            list.Add(new List<int> { nums[0] });
+            
+            for (int i = 1; i < nums.Length; i++)
+            {
+                var temp = new List<IList<int>>();
+                temp.AddRange(list);
+                list.Clear();
+
+                foreach (var item in temp)
+                {
+                    for (int pos = 0; pos <= item.Count; pos++)
+                    {
+                        var cur = new List<int>();
+                        cur.AddRange(item);
+                        cur.Insert(pos, nums[i]);
+                        list.Add(cur);
+                    }
+                }
             }
 
             return list;
         }
+
     }
 }
