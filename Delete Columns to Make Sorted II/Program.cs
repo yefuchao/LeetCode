11a4673be0ae61a09c9c1c9b27e5a1b54f3e4d24
yefuchao <ye0531@live.com>
2019-09-03 14:20:17 +0800

    TODO

---
+++ Delete Columns to Make Sorted II/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delete_Columns_to_Make_Sorted_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //expect answer 1
+            Console.WriteLine(MinDeletionSize(new string[] { "xga", "xfb", "yfa" }));
+
+            Console.ReadLine();
+        }
+
+
+        public static int MinDeletionSize(string[] A)
+        {
+            return 0;
+        }
+    }
+}
---
+++ NumTilePossibilities/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumTilePossibilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Console.WriteLine(NumTilePossibilities2("AAB"));
+
+            Console.ReadLine();
+        }
+
+        //Brilliant solution!
+        public static int NumTilePossibilities2(string tiles)
+        {
+            int[] count = new int[26];
+
+            var chararrays = tiles.ToCharArray();
+
+            //统计字母个数
+            foreach (var item in chararrays)
+            {
+                count[item - 'A']++;
+            }
+
+            return dfs(count);
+
+        }
+
+        static int dfs(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                if (arr[i] == 0) continue;
+                sum++;
+                arr[i]--;
+                sum += dfs(arr);
+                arr[i]++;
+            }
+            return sum;
+        }
+
+        //Time Limit Exceeded
+        public static int NumTilePossibilities(string tiles)
+        {
+            var list = new List<string>();
+
+            var chars = tiles.ToCharArray();
+
+            //Array.Sort(chars);
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var curList = new List<string>();
+                curList.AddRange(list);
+
+                var cur = chars[i].ToString();
+
+                if (!curList.Contains(cur))
+                {
+                    curList.Add(cur);
+                    //Console.WriteLine(cur);
+                }
+
+                foreach (var item in list)
+                {
+                    for (int j = 0; j <= item.Length; j++)
+                    {
+                        var newstring = item.Insert(j, cur);
+
+                        if (!curList.Contains(newstring))
+                        {
+                            curList.Add(newstring);
+                            //Console.WriteLine(newstring);
+                        }
+                    }
+                }
+
+                list.Clear();
+                list.AddRange(curList);
+
+            }
+
+            return list.Count;
+        }
+    }
+}
