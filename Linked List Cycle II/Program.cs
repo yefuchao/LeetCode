994575f5e88e1412ea3969ba7ed1cff62213481c
yefuchao <ye0531@live.com>
2019-10-25 18:09:44 +0800

    Rotate List

---
+++ Linked List Cycle II/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Linked_List_Cycle_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class ListNode
+        {
+            public int val { get; set; }
+
+            public ListNode next { get; set; }
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+        }
+    }
+}
--- Rotate List/Program.cs
+++ Rotate List/Program.cs
@@ -9,13 +9,48 @@ namespace Rotate_List
             Console.WriteLine("Hello World!");
             ListNode l = new ListNode(1);
             l.next = new ListNode(2);
-            l.next.next = new ListNode(3);
-            l.next.next.next = new ListNode(4);
-            l.next.next.next.next = new ListNode(5);
+            //l.next.next = new ListNode(3);
+            //l.next.next.next = new ListNode(4);
+            //l.next.next.next.next = new ListNode(5);
 
-            var r =  RotateRight(l, 2);
+            var r = R(l, 0);
 
             Console.ReadKey();
+        }
+
+        public static ListNode R(ListNode head, int k)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            int length = 1;
+            ListNode e = head;
+            while (e.next != null)
+            {
+                length++;
+                e = e.next;
+            }
+
+            k = k % length;
+
+            int p = length - k - 1;
+
+            ListNode n = head;
+
+            while (p > 0)
+            {
+                n = n.next;
+                p--;
+            }
+
+            e.next = head;
+            ListNode res = n.next;
+            
+            n.next = null;
+            return res;
+
         }
 
         public static ListNode RotateRight(ListNode head, int k)
@@ -32,6 +67,7 @@ namespace Rotate_List
 
             ListNode old_tail = head;
 
+            //长度
             int n;
 
             for (n = 1; old_tail.next != null; n++)
