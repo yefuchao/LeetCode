49ec1db0787a3674708840911ee623223972b26a
alex <ye0531@live.com>
2019-10-27 10:57:17 +0800

    ago

---
+++ Distance Between Bus Stops/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Distance_Between_Bus_Stops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var distance = new int[] { 1, 2, 3, 4 };
+
+
+            var r = DistanceBetweenBusStops(distance, 0, 2);
+            Console.WriteLine(r);
+
+
+            Console.ReadLine();
+        }
+
+        public static int DistanceBetweenBusStops(int[] distance, int start, int destination)
+        {
+            var n = distance.Length;
+
+            if (start > destination)
+            {
+                var p = start;
+                start = destination;
+                destination = p;
+            }
+
+            var way1 = 0;
+            for (int i = start; i < destination; i++)
+            {
+                way1 += distance[i];
+            }
+
+            var way2 = 0;
+
+            for (int i = 0; i < start; i++)
+            {
+                way2 += distance[i];
+            }
+
+            for (int i = destination; i < n; i++)
+            {
+                way2 += distance[i];
+            }
+
+
+            return way1 > way2 ? way2 : way1;
+        }
+    }
+}
---
+++ Maximum Subarray Sum with One Deletion/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Maximum_Subarray_Sum_with_One_Deletion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 3, -4, -4, 2, 5, -1, 5 };
+
+            Console.WriteLine(MaximumSum2(arr));
+
+            Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int MaximumSum(int[] arr)
+        {
+            int max = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var sum = arr[i];
+                var min = 0;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < 0)
+                    {
+                        min = Math.Min(arr[j], min);
+                    }
+
+                    sum += arr[j];
+
+                    max = Math.Max(max, sum - min);
+                }
+
+                max = Math.Max(max, sum);
+            }
+
+            return max;
+        }
+
+        public static int MaximumSum2(int[] arr)
+        {
+            int max = arr[0];
+            var min = 0;
+
+            var pre = new int[arr.Length];
+
+            pre[0] = max;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                var cur = arr[i];
+
+
+
+                pre[i] = Math.Max(cur, pre[i - 1] + cur);
+
+                if (pre[i] > max)
+                {
+                    max = pre[i];
+
+                    if (pre[i] < 0)
+                    {
+                        min = Math.Min(min, pre[i]);
+                    }
+                   
+                }
+
+            }
+
+            return max;
+        }
+    }
+}
