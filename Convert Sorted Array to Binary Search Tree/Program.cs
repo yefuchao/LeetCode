5a96dc350a2878d8a5b692c87a8df8512d85df1d
alex <ye0531@live.com>
2019-10-29 22:26:26 +0800

    160 weekly contest &108 Convert Sorted Array to Binary Search Tree

---
+++ Convert Sorted Array to Binary Search Tree/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Convert_Sorted_Array_to_Binary_Search_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] nums = new int[] { -10, -3, 0, 1, 9 };
+
+
+            SortedArrayToBST(nums);
+
+            Console.ReadKey();
+        }
+
+        public static TreeNode SortedArrayToBST(int[] nums)
+        {
+
+            if (nums.Length == 0)
+            {
+                return null;
+            }
+
+            return BuildTree(nums, 0, nums.Length-1);
+
+        }
+
+        public static TreeNode BuildTree(int[] nums, int start, int end)
+        {
+            int mid = (end + start) / 2;
+            TreeNode tree = new TreeNode(nums[mid]);
+            if (start <= mid - 1)
+            {
+                tree.left = BuildTree(nums, start, mid - 1);
+            }
+            if (mid + 1 <= end)
+            {
+                tree.right = BuildTree(nums, mid + 1, end);
+            }
+            return tree;
+        }
+
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+}
+
+
---
+++ Tiling a Rectangle with the Fewest Squares/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Tiling_a_Rectangle_with_the_Fewest_Squares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //correct 6
+            var min = TilingRectangle(11, 13);
+
+            Console.WriteLine(min);
+            Console.ReadKey();
+
+        }
+
+        public static int TilingRectangle(int n, int m)
+        {
+            if (n == m)
+            {
+                return 1;
+            }
+
+            int x = Math.Min(n, m);
+
+            int min = m * n;
+
+            for (int i = 7; i <= x; i++)
+            {
+                min = Math.Min(min, 1 + TilingRectangle(i, n - i) + TilingRectangle(n, m - i));
+            }
+
+            return min;
+
+        }
+
+        //public static int Find(int s, int n)
+        //{
+        //    int ans = s;
+
+        //    for (int i = 1; i < n; i++)
+        //    {
+        //        ans = Math.Min(ans,1 + )
+        //    }
+        //}
+    }
+}
