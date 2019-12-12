3f3d4a9cf5bb1957d5e4cb120d686cfa7df999ea
yefuchao <ye0531@live.com>
2019-12-12 17:02:56 +0800

    #63 Unique Paths II

---
+++ Unique Paths II/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Unique_Paths_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int UniquePathsWithObstacles(int[][] obstacleGrid)
+        {
+            int m = obstacleGrid.Length;
+            int n = obstacleGrid[0].Length;
+
+            int[,] dp = new int[m + 1, n + 1];
+
+
+            dp[0, 1] = 1;
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    //路障,此路不通
+                    if (obstacleGrid[i][j] == 1)
+                    {
+                        dp[i + 1, j + 1] = 0;
+                    }
+                    else
+                    {
+                        dp[i + 1, j + 1] = dp[i, j + 1] + dp[i + 1, j];
+                    }
+                }
+            }
+
+            return dp[m, n];
+
+        }
+
+    }
+}
