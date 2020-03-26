07d24f4a413994da5d76e5bceaf7d17c85c1c727
yefuchao <ye0531@live.com>
2020-03-26 11:52:14 +0800

    338. Counting Bits

---
+++ Counting Bits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Counting_Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int[] CountBits(int num)
+        {
+            int[] ans = new int[num + 1];
+            ans[0] = 0;
+
+            int sqrt = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                if (i >= sqrt * 2)
+                {
+                    sqrt = sqrt * 2;
+                }
+                var k = i - sqrt;
+                ans[i] = 1 + ans[k];
+            }
+
+            return ans;
+        }
+    }
+}
